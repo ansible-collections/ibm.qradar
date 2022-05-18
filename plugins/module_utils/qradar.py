8dbb8da1c7c233adfdaddac701fff35c62e77aa7
Gonéri Le Bouder <goneri@lebouder.net>
2022-05-18 11:19:00 -0400

    DNM CI TESTING

--- plugins/module_utils/qradar.py
+++ plugins/module_utils/qradar.py
@@ -4,9 +4,6 @@
 # (c) 2019, Adam Miller (admiller@redhat.com)
 # GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
 
-from __future__ import absolute_import, division, print_function
-
-__metaclass__ = type
 from ansible.module_utils.urls import CertificateError
 from ansible.module_utils.six.moves.urllib.parse import quote_plus
 from ansible.module_utils.connection import ConnectionError
