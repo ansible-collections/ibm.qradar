cd04e81af6bfd941d97b08dbff3d3a17e53a649c
Sumit Jaiswal <sjaiswal@redhat.com>
2022-05-24 13:39:44 +0530

    To fix doc for fixing the sanity failures (#52)
    To fix doc for fixing the sanity failures
    
    SUMMARY
    
    To fix doc for fixing the sanity failures
    
    ISSUE TYPE
    
    
    Docs Pull Request
    
    COMPONENT NAME
    
    qradar
    ADDITIONAL INFORMATION

--- plugins/httpapi/qradar.py
+++ plugins/httpapi/qradar.py
@@ -7,13 +7,13 @@ __metaclass__ = type
 
 DOCUMENTATION = """
 ---
-author: Ansible Security Automation Team
-httpapi : qradar
+author: Ansible Security Team (@ansible-security)
+name: qradar
 short_description: HttpApi Plugin for IBM QRadar
 description:
   - This HttpApi plugin provides methods to connect to IBM QRadar over a
     HTTP(S)-based api.
-version_added: "1.0"
+version_added: "1.0.0"
 """
 
 import json
